2b3e197181862d49e91bbd4c2359bb83dd8dc472
Daniel Johansson <johansson.gd@gmail.com>
2018-11-22 21:08:08 +0100

    Start of presentation layer

--- HandelU/HandelU/Program.cs
+++ HandelU/HandelU/Program.cs
@@ -10,6 +10,23 @@ namespace HandelU
     {
         static void Main(string[] args)
         {
+            // Welcome menu
+            Console.WriteLine("****** Welcome to the Item store! ******");
+            Console.WriteLine("\n- To list available items, press \"i\"");
+            Console.WriteLine("- To buy, write the number of the item you want");
+            Console.WriteLine("- To exit the store, press \"q\"\n");
+
+            // Promt user for input
+            while (true)
+            {
+                string uInput = Console.ReadLine();
+
+                // Exit programme
+                if (uInput.ToLower() == "q")
+                {
+                    break;
+                }
+            }
         }
     }
 }
